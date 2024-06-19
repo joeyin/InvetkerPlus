de580c6534c2d4182cc9d72a8dbc97c97b99e8b0
s60621tw <s60621tw@Humber>
2024-06-19 17:17:42 -0400

    Update UserModel

--- Invetker/Controllers/AccountController.cs
+++ Invetker/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Invetker.Models;
+using System.Diagnostics;
 
 namespace Invetker.Controllers
 {
