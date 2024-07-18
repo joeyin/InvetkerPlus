2ff7235e8043e4d3e222d768a1a4e96fa372b064
s60621tw <s60621tw@Humber>
2024-07-18 13:31:50 -0400

    test

--- Invetker/Models/PricesModels.cs
+++ Invetker/Models/PricesModels.cs
@@ -32,10 +32,5 @@ namespace Invetker.Models
         public decimal Price { get; set; }
 
         public DateTime Timestamp { get; set; }
-
-        public string color { get; set; }
-
-        public string text { get; set; }
-
     }
 }
